3bb10c801afb766248254ea4882e0a3a033ba1ef
devlooppear <iago.profissional.developer@gmail.com>
2024-01-21 16:13:49 -0300

    api

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,36 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\PermissionController;
+use App\Http\Controllers\ReservationController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::middleware('auth:sanctum')->group(function () {
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    // Books
+    Route::prefix('books')->group(function () {
+        Route::apiResource('books', BookController::class);
+    });
+
+    // Permissions
+    Route::prefix('permissions')->group(function () {
+        Route::apiResource('permissions', PermissionController::class);
+    });
+
+    // Reservations
+    Route::prefix('reservations')->group(function () {
+        Route::apiResource('reservations', ReservationController::class);
+    });
+
+    // Roles
+    Route::prefix('roles')->group(function () {
+        Route::apiResource('roles', RoleController::class);
+    });
+
+    // Users
+    Route::prefix('users')->group(function () {
+        Route::apiResource('users', UserController::class);
+    });
 });
